9d9cfcc0b2c7c1dc86df0350158b621ec33d823f
aninwan <sasuketeme14@gmail.com>
2021-06-04 10:34:08 +0700

    login_page

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,29 +1,144 @@
-class Login {
-  int _id;
-  String _username;
-  String _password;
+import 'package:flutter/material.dart';
 
-  int get id => this._id;
 
-  String get username => this._username;
-  set username(String value) => this._username = value;
+class LoginPage extends StatefulWidget {
+  @override
+  
+  _LoginPageState createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-  String get password => this._password;
-  set password(String value) => this._password = value;
-
-  Login(this._username, this._password);
-
-  Login.fromMapLogin(Map<String, dynamic> map) {
-    this._id = map['id'];
-    this._username = map['username'];
-    this._password = map['password'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlutterLogo(size: 150),
+              SizedBox(height: 50),
+              Container(
+                margin: EdgeInsets.all(10),
+                width: 350,
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: 'Email Address',
+                        icon: Icon(Icons.email),
+                      ),
+                    ),
+                    TextField(
+                      obscureText: true,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        icon: Icon(Icons.input),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      child: OutlineButton(
+                        splashColor: Colors.grey,
+                        onPressed: () {
+                          signInWithEmail(
+                                  emailController.text, passwordController.text)
+                              .then((result) {
+                            if (result != null) {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return FirstScreen();
+                                  },
+                                ),
+                              );
+                            }
+                          });
+                        },
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40)),
+                        highlightElevation: 0,
+                        borderSide: BorderSide(color: Colors.grey),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: Text(
+                                  'Log in ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-  Map<String, dynamic> toMapLogin() {
-    Map<String, dynamic> map = Map<String, dynamic>();
-    map['id'] = this._id;
-    map['username'] = username;
-    map['password'] = password;
-    return map;
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        signInWithGoogle().then((result) {
+          if (result != null) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return FirstScreen();
+                },
+              ),
+            );
+          }
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            /*Image(
+                image: NetworkImage(
+                    'https://www.freepnglogos.com/uploads/google-logo-png/google-logo-icon-png-transparent-background-osteopathy-16.png'),
+                height: 35.0),*/
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google ',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.blue,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

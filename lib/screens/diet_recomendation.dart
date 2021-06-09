e98506342bac4d26eeb13f2f3023343c08f233e7
aninwan <sasuketeme14@gmail.com>
2021-06-10 01:15:02 +0700

    session2

--- lib/screens/diet_recomendation.dart
+++ lib/screens/diet_recomendation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:meditation_app/constants.dart';
 import 'package:meditation_app/subScreenDiet/session1.dart';
+import 'package:meditation_app/subScreenDiet/session2.dart';
 import 'package:meditation_app/widgets/bottom_nav_bar.dart';
 //import 'package:meditation_app/widgets/search_bar.dart';
 
@@ -72,7 +73,14 @@ class DietRecomendation extends StatelessWidget {
                         SeassionCard(
                           //session2
                           seassionNum: 2,
-                          press: () {},
+                          press: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return DietVegan();
+                              }),
+                            );
+                          },
                         ),
                         SeassionCard(
                           //session3
